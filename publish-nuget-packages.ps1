0a3ae8bd7880e15d0b29787dd5b5221b18eac26c
Joshua Sim <joshzx@gmail.com>
2015-02-20 01:40:12 +1100

    =Added nuget publish ps

---
+++ publish-nuget-packages.ps1
@@ -0,0 +1,30 @@
+$keyfile = "C:\NugetKey\nuget-access-key.txt"
+$scriptpath = split-path -parent $MyInvocation.MyCommand.Path
+$nugetpath = resolve-path "$scriptpath/../lib/nuget/nuget.exe"
+$packagespath = resolve-path "$scriptpath/../build/packages"
+ 
+if(-not (test-path $keyfile)) {
+  throw "Could not find the NuGet access key at $keyfile. If you're not Josh, you shouldn't be running this script!"
+}
+else {
+  pushd $packagespath
+ 
+  # get our secret key. This is not in the repository.
+  $key = get-content $keyfile
+ 
+  # Find all the packages and display them for confirmation
+  $packages = dir "*.nupkg"
+  write-host "Packages to upload:"
+  $packages | % { write-host $_.Name }
+ 
+  # upload
+  elseif($result -eq 1) {
+    $packages | % { 
+        $package = $_.Name
+        write-host "Uploading $package"
+        & $nugetpath push -source "http://packages.nuget.org/v1/" $package $key
+        write-host ""
+    }
+  }
+  popd
+}
